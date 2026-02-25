7ad6fd9edc801eac5b60b05ef3c71b111c581a09
Melih Alan <157570511+hmelihalan@users.noreply.github.com>
2026-02-25 12:57:09 +0300

    Update FindMissingElements.py

--- easy/FindMissingElements.py
+++ easy/FindMissingElements.py
@@ -1,3 +1,12 @@
+'''You are given an integer array nums consisting of unique integers.
+
+Originally, nums contained every integer within a certain range. However, some integers might have gone missing from the array.
+
+The smallest and largest integers of the original range are still present in nums.
+
+Return a sorted list of all the missing integers in this range. If no integers are missing, return an empty list.
+'''
+
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         missing_nums = []
